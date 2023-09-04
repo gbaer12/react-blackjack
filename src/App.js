75b9a8a80565179b0e1a9579fb5f1088d776bdd8
gbaer <gbaer@EYI-PC>
2023-09-04 05:45:24 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import BlackJackGame from "./BlackjackGame";
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <BlackJackGame />
+    </div>
+  );
+}
+
+export default App;
